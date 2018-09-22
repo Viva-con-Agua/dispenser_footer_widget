b12508292f1c8809402f4dbf0b090bc5ebd14ff8
Dennis Kleber <dennis_kleber@mailbox.org>
2018-09-22 12:31:25 +0200

    setup footer as widget

---
+++ src/Footer.vue
@@ -0,0 +1,25 @@
+<template>
+<div class="footer-container">
+			<div class="footer-row">
+				<div class="footer-left">
+					<p class="screen-talign-left">
+						<a title="Die Nutzungsbedingungen lesen" href="https://pool.vivaconagua.org/nutzungsbedingungen/">Nutzungsbedingungen</a>
+							<br />
+						<span class="footer-link"></span>
+						<a title="Die Datenschutzerklärung lesen" href="https://pool.vivaconagua.org/datenschutzerklaerung/">Datenschutzerklärung</a>
+							<br />
+						<span class="footer-link"></span>
+						<a title="Zum Impressum" href="/imprint">Impressum</a>
+					</p>
+				</div>
+				<div class="footer-right">
+				</div>
+			</div>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'Footer'
+}
+</script>
